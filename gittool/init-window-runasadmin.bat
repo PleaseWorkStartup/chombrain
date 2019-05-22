fb8898d36468a7d4980b5ede6cef9dc2f0f0c388
Chomtana <Chomtana001@gmail.com>
2019-05-22 20:36:36 +0700

    Update

--- gittool/init-window-runasadmin.bat
+++ gittool/init-window-runasadmin.bat
@@ -5,7 +5,7 @@ cd ..
 git remote add plannee-chombrain https://github.com/PleaseWorkStartup/plannee-chombrain
 git remote add chombrain https://github.com/PleaseWorkStartup/chombrain
 
-git subtree pull --prefix plannee-chombrain plannee-chombrain master --squash
-git subtree pull --prefix plannee-chombrain/chombrain chombrain master --squash
+git subtree pull --prefix plannee-chombrain plannee-chombrain master
+git subtree pull --prefix plannee-chombrain/chombrain chombrain master
 
 pause
