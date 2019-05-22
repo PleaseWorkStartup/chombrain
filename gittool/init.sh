4c8818b1c28647df2a90513fa1e12052b8f9f8de
Chomtana <Chomtana001@gmail.com>
2019-05-22 20:31:06 +0700

    Reinit

---
+++ gittool/init.sh
@@ -0,0 +1,7 @@
+cd ..
+
+git remote add chombrain https://github.com/PleaseWorkStartup/chombrain
+
+git subtree pull --prefix chombrain chombrain master --squash
+
+read -p "Press Enter to Continue"
